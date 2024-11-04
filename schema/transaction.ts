d5385fa7748802c1205acf5154ac2bfaf50b67db
Sam Marxz <sammarxzz@gmail.com>
2024-11-04 19:44:11 -0300

    fix category nullable

--- schema/transaction.ts
+++ schema/transaction.ts
@@ -4,7 +4,7 @@ export const CreateTransactionSchema = z.object({
   amount: z.coerce.number().positive().multipleOf(0.01),
   description: z.string().min(2),
   date: z.coerce.date(),
-  category: z.string(),
+  category: z.string().min(1, "Category is required"),
   type: z.union([z.literal("income"), z.literal("expense")]),
 });
 
