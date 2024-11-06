8129bdaa06cc72ab531a883b9b29d1ad6e30bcea
Sam Marxz <sammarxzz@gmail.com>
2024-11-06 18:39:41 -0300

    return null if user don't have created any transaction

--- components/categories/CategoriesStats.tsx
+++ components/categories/CategoriesStats.tsx
@@ -64,6 +64,10 @@ interface CategoriesCardProps {
 function CategoriesCard({ formatter, type, data }: CategoriesCardProps) {
   const filteredData = data.filter((item) => item.type === type);
 
+  if (filteredData.length === 0) {
+    return null;
+  }
+
   return (
     <Card className="border-none shadow-none p-0">
       <CardHeader className="px-0  pt-0 pb-6">
@@ -75,17 +79,6 @@ function CategoriesCard({ formatter, type, data }: CategoriesCardProps) {
       </CardHeader>
       <CardContent className="p-0">
         <div className="space-y-4">
-          {filteredData.length === 0 && (
-            <div className="flex h-fit max-h-60 w-full flex-col items-center justify-center">
-              <p className="text-base text-muted-foreground">
-                No data to show for this period
-              </p>
-              <p className="text-sm">
-                try selecting a different date range or adding a new transaction
-              </p>
-            </div>
-          )}
-
           <ScrollArea className="h-fit max-h-60 w-full">
             <div className="flex w-full flex-col gap-4">
               <BarList
